13511745f99b67b1e16b981c6ce58b10e936ab29
karni prabhu <karni.prabhu@techverito.com>
2026-01-07 19:18:24 +0530

    fix the landing page

--- home/views.py
+++ home/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from books.models import Book
 
 def index(request):
     template_data = {}
     template_data['title'] = 'Books Store'
+    template_data['books'] = Book.objects.all().order_by('-id')[:3]
     return render(request, 'home/index.html', {'template_data': template_data})
 
 def about(request):
